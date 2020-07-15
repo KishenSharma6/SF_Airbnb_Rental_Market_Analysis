a7655f45365e3330df726874a24dd62cd4cacd9f
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-07-15 11:06:30 -0700

    Added return dict function

--- Project_Codes/04_Modeling/Regression_Metrics.py
+++ Project_Codes/04_Modeling/Regression_Metrics.py
@@ -1,7 +1,15 @@
 import numpy as np
 from sklearn.metrics import r2_score, mean_absolute_error
 
-def regression_metrics(name,y_hat, y):
+def regression_metrics(name,y_hat, y,return_dict = True):
+    """
+    Prints regression metrics given:
+    name: model name as a string
+    y_hat: predictions
+    y = actual
+    
+    Also returns dictionary of metrics if return_dict = True
+    """
     mae = mean_absolute_error(y, y_hat)
     mse = np.mean(np.square((y - y_hat)))
     rmse = np.sqrt(mse)
@@ -9,3 +17,8 @@ def regression_metrics(name,y_hat, y):
     print('{} Evaluation Metrics:'.format(name))
     print('R2: {}\nMAE: {} \nMSE: {}\nRMSE: {}'.format(r2,mae,mse,rmse))
     print('==================================================')
+    if return_dict == True:
+        metrics_dict = {}
+        for metric in ('r2','mae','mse','rmse'):
+            metrics_dict[metric] = locals()[metric]
+        return metrics_dict
